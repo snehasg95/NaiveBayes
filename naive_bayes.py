327039baf880daf36284cadb0db730e6b18d895e
SnehaPriya <31264991+snehasg95@users.noreply.github.com>
2019-12-09 15:00:15 -0800

    Naive Bayes to classify iris flowers

---
+++ naive_bayes.py
@@ -0,0 +1,63 @@
+''' Here we use the iris data set from here : https://archive.ics.uci.edu/ml/datasets/iris 
+involving classification of different types of iris flowers '''
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+
+iris = datasets.load_iris()
+class_names = iris.target_names
+iris_df = pd.DataFrame(iris.data, columns='iris.feature_names')
+iris_df['target'] = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.25, random_state=0)
+''' To test shapes
+
+print(iris_df.shape)
+print(X_train.shape)
+print(X_test.shape)
+
+print(y_train.shape)
+print(y_test.shape)
+
+
+'''
+
+clf = GaussianNB()
+clf.fit(X_train, y_train)
+
+y_predict = clf.predict(X_test)
+
+score = accuracy_score(y_predict, y_test)
+print(score)
+#score = np.mean(y_predict == y_test)
+
+
+
+''' Instead of splitting data like above one can also,
+
+X_train = iris_df.drop(columns=['target'],axis=1)
+y_train = iris_df['target']
+
+# seperate the independent and target variable on testing data
+X_test = iris_df.drop(columns=['target'],axis=1)
+y_test = iris_df['target']
+
+clf = GaussianNB()
+clf.fit(X_train, y_train)
+
+y_predict = clf.predict(X_test)
+print(np.mean(y_predict == y_test))
+# score = accuracy_score(y_predict, y_test)
+# print(score) 
+
+'''
+
+
+
