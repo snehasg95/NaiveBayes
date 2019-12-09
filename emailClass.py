ae6ad1788cd47b2636ae03a4716e4daad7ade078
SnehaPriya <31264991+snehasg95@users.noreply.github.com>
2019-12-09 15:02:37 -0800

    classify emails or text files

---
+++ emailClass.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
